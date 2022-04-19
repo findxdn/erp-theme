a895cff936c55969d558c8fe3d4269c144d07537
DinhNT <dinhtrum@gmail.com>
2022-04-19 09:53:17 +0700

    init project

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Button.scss";
+
+export interface ButtonProps {
+  label: string;
+  onClick?: any;
+  children?: any;
+  className?: any;
+  disabled?: any;
+  sx?: any;
+  icon?: any;
+  type?: any;
+}
+
+const Button = (props: ButtonProps) => {
+  const { onClick, children, className, sx, disabled, icon, type = 'button' } = props
+  return (
+    <button
+      type={type}
+      style={sx}
+      onClick={onClick}
+      disabled={disabled}
+      className={`button-default ${!!className ? className : ''}`}
+    >
+      {icon ? <span className="icon-style">{!!icon && icon}</span> : <></>}
+      {children}
+    </button>
+  )
+};
+
+export default Button;
